c5b71f72fbb50f1471b89d69789019d449eb6ca0
Estanislao Ruiz <eruiz@plantium.com>
2020-04-18 21:12:58 -0300

    Improve Consulta schema

--- models/patient_quest_model.js
+++ models/patient_quest_model.js
@@ -3,26 +3,26 @@ const Schema = mongoose.Schema;
 
 const PatientQuest = new Schema(
     {
-        nombre: { type: String, required: true },
-        apellido: { type: String, required: true },
+        nombreCompleto: { type: String, required: true },
         email: { type: String, required: true },
-        edad: { type: Number, required: true },
+        edad: { type: String, required: true },
         sexo: { type: String, required: true },
-        dni: { type: Number, required: true },
-        tel: { type: String, required: true },
+        dni: { type: String, required: true },
+        domicilio: { type: String, required: true },
+        telefono: { type: String, required: true },
         consulta: { type: String, required: false },
         preg_medicas: {
-                fiebre: { type: Boolean, required: true },
-                tos: { type: Boolean, required: true },
-                dolor_garganta: { type: Boolean, required: true },
-                problema_respiracion: { type: Boolean, required: true },
-                perdida_olfato: { type: Boolean, required: true },
-                perdida_gusto: { type: Boolean, required: true },
+                fiebre: { type: Boolean, required: false },
+                tos: { type: Boolean, required: false },
+                dolor_garganta: { type: Boolean, required: false },
+                problema_respiracion: { type: Boolean, required: false },
+                perdida_olfato: { type: Boolean, required: false },
+                perdida_gusto: { type: Boolean, required: false },
         },
-        nro_turno: { type: Number, required: true },
-        activo: { type: Boolean, required: true },
+        nro_turno: { type: Number, required: false, default: -1 },
+        activo: { type: Boolean, required: false, default: false},
     },
     { timestamps: true },
 );
 
-module.exports = mongoose.model('patient_quest', PatientQuest);
+module.exports = mongoose.model('Consulta', PatientQuest);

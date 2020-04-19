606b0e558c862c4028034bf1257c7fd633a7b417
Estanislao Ruiz <eruiz@plantium.com>
2020-04-19 00:04:52 -0300

    Add post req for zoom session

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const crypto = require('crypto');
+
 
 const db = require('./db');
 const movieRouter = require('./routes/doc_quest_router');
@@ -9,13 +12,25 @@ const app = express();
 const apiPort = 3333;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors())
+app.options('*', cors());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+app.post('/zoom', (req, res) => {
+
+    const timestamp = new Date().getTime() - 30000;
+    const msg = Buffer.from(process.env.API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64');
+    const hash = crypto.createHmac('sha256', process.env.API_SECRET).update(msg).digest('base64');
+    const signature = Buffer.from(`${process.env.API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64');
+
+        res.json({
+        signature: signature
+    })
 });
 
 app.use('/api', movieRouter);
